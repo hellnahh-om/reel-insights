e1a4c630e6aca266382760a9678f1016e217e04f
hellnahh-om <gautamom118@gmail.com>
2026-07-21 00:21:56 +0530

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReelInsightsEditor from "./ReelInsightsEditor.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ReelInsightsEditor />
+  </React.StrictMode>
+);
